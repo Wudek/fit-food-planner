e067c5d4621bb463ec0ae9d68dd24a954c9a7525
paul.reinlein <paul.reinlein@gmail.com>
2014-04-25 20:30:31 -0400

    a30

---
+++ src/server/db/ClientController.js
@@ -0,0 +1,23 @@
+var Client = require("./dbSchema").Client;
+
+module.exports.createClient = function (name, male, height, weight)
+{
+	var clientObj = new Client(
+		{
+			name   : name,
+			male   : male,
+			height : height,
+			weight : weight,
+			diet   : {
+				meals : []
+			}
+		});
+	clientObj.save();
+};
+
+module.exports.listClients = function()
+{
+	Client.find().exec(function(err, clients)
+	{
+	})
+};
