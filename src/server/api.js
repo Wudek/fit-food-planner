30ac1eadc0a7df313af4ed6d72b081e428874e0c
paul.reinlein <paul.reinlein@gmail.com>
2014-04-30 17:04:09 -0400

    a32

--- src/server/api.js
+++ src/server/api.js
@@ -1,7 +1,9 @@
 'use strict';
-/*
- * GET users listing.
- */
+
+var clientController = require('db/clientController');
+var foodController = require('db/FoodController');
+var logger = require('./logger');
+var Promise = require('bluebird');
 
 exports.list = function(req, res){
   res.send('respond with a resource');
